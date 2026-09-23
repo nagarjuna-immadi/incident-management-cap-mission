2152e63300fe31f4c61eb4e52c1d59b937a78459
Nagarjuna Immadi <nagarjuna.immadi89@gmail.com>
2026-09-23 17:26:56 +0000

    first commit

---
+++ srv/services.js
@@ -0,0 +1,22 @@
+import cds from '@sap/cds'
+
+export class ProcessorService extends cds.ApplicationService {
+  /** Registering custom event handlers */
+  init() {
+    this.before("UPDATE", "Incidents", (req) => this.onUpdate(req));
+    this.before(["CREATE", "UPDATE"], "Incidents", (req) => this.changeUrgencyDueToSubject(req.data));
+
+    return super.init();
+  }
+
+  changeUrgencyDueToSubject(data) {
+    let urgent = data.title?.match(/urgent/i)
+    if (urgent) data.urgency_code = 'H'
+  }
+
+  /** Custom Validation */
+  async onUpdate (req) {
+    let closed = await SELECT.one(1) .from (req.subject) .where `status.code = 'C'`
+    if (closed) req.reject `Can't modify a closed incident!`
+  }
+}
